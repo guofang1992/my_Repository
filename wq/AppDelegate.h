1d3ae8da82068c756f75bae45c5005c0042e291f
kaixin <kaixin@kaixindemac-mini.local>
2017-03-28 14:24:17 +0800

    Initial Commit

---
+++ wq/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wq
+//
+//  Created by kaixin on 2017/3/28.
+//  Copyright © 2017年 kaixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
